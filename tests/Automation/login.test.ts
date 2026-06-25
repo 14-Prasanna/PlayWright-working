6261e055d70f0893a5fd6158dbd8182838dce024
14-Prasanna <2k22cse115@kiot.ac.in>
2026-06-25 22:26:04 +0530

    About today 25/06/2026

---
+++ tests/Automation/login.test.ts
@@ -0,0 +1,13 @@
+import {test, expect } from '@playwright/test';
+
+test('Login Test', async ({page}) => {
+    await page.goto("http://automationexercise.com");
+    await expect(page.locator("//img[@alt='Website for automation practice']")).toBeVisible();
+    await page.getByText(" Signup / Login").click();
+    await page.locator("//input[@data-qa='login-email']").fill("testlogin@gmail.com");
+    await page.getByPlaceholder("Password").fill("1234567890");
+    await page.getByRole('button',{name: 'Login'}).click();
+    await expect(page.getByText('Logged in as Test cases')).toBeVisible();
+    await page.getByRole('link', {name: ' Logout'}).click();
+    await expect(page).toHaveURL('https://automationexercise.com/login'); 
+});
