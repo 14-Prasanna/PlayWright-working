437e1d59aefe90706dbe449b1ec69dc7cb98137f
14-Prasanna <2k22cse115@kiot.ac.in>
2026-06-30 06:33:18 +0530

    Error corrected

--- tests/describe.test.ts
+++ tests/describe.test.ts
@@ -1,56 +1,54 @@
 import { test, expect } from '@playwright/test';
 
+test.describe("Dummy @Smoke", () => {
 
-test.describe("Dummy @Smoke", () =>{
+  test("Summa", async ({ page }) => {
+    console.log("This is dummy describe");
+  });
 
-    test("Summa", async({page}) => {
-        console.log("This is dummy describe");
-    })
 });
 
+test.describe('Login Module @Login', () => {
 
-test.describe.only('Login Module @Login', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+  });
 
-    test.beforeEach(async ({page}) => {
-        await page.goto("https://the-internet.herokuapp.com/login")
-    })
+  test.describe("valid", { tag: ["@Smoke", "@Regression"] }, () => {
 
-    test.describe("valid", {tag: ["@Smoke", "@Regression"]}, () => {
+    test('Valid Login Test', async ({ page }, testInfo) => {
 
-        test('Valid Login Test', async ({ page }, testInfo) => {
-            
-        console.log(`Test Title, ${testInfo.title}`);
-        
-        await page.fill('#username', "tomsmith");
-        await page.fill('#password', "SuperSecretPassword!");
-        await page.click('button[type="submit"]');
+      console.log(`Test Title: ${testInfo.title}`);
 
-        await expect(page.locator('#flash'))
-            .toContainText('You logged into a secure area!');
+      await page.fill('#username', "tomsmith");
+      await page.fill('#password', "SuperSecretPassword!");
+      await page.click('button[type="submit"]');
 
-        await expect(page)
-            .toHaveURL('https://the-internet.herokuapp.com/secure');
-        });
+      await expect(page.locator('#flash'))
+        .toContainText('You logged into a secure area!');
 
-    })
-
-    test.describe("Invalid @Regression", () => {
-
-        test('Invalid Login Test', async ({ page }) => {
-
-        await page.fill('#username', "Admin");
-        await page.fill('#password', "admin123");
-        await page.click('button[type="submit"]');
-
-        await expect(page.locator('#flash'))
-            .toContainText('Your username is invalid!');
-        });
-
-
+      await expect(page)
+        .toHaveURL('https://the-internet.herokuapp.com/secure');
     });
 
-    test.afterEach(async ({page}) => {
-        console.log("Test is Completed")
-    })
+  });
+
+  test.describe("Invalid @Regression", () => {
+
+    test('Invalid Login Test', async ({ page }) => {
+
+      await page.fill('#username', "Admin");
+      await page.fill('#password', "admin123");
+      await page.click('button[type="submit"]');
+
+      await expect(page.locator('#flash'))
+        .toContainText('Your username is invalid!');
+    });
+
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("Test is Completed");
+  });
 
 });
